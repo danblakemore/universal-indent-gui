94de8c9be884b8cac88602f9043c796d319c0c13
thomas_-_s <thomas_-_s@59b1889a-e5ac-428c-b0c7-476e01d41282>
2007-01-31 07:39:34 +0000

    This build file is not needed anymore. Was only used to build with nmake via commandline.
    git-svn-id: svn://svn.code.sf.net/p/universalindent/code/trunk@188 59b1889a-e5ac-428c-b0c7-476e01d41282

--- makeall.bat
+++
@@ -1,5 +0,0 @@
-qmake
-nmake clean
-nmake all
-rem mt -manifest .\release\UniversalIndentGUI.exe.manifest -outputresource:".\release\UniversalIndentGUI.exe;#1"
-rem mt -manifest .\debug\UniversalIndentGUI.exe.manifest -outputresource:".\debug\UniversalIndentGUI.exe;#1"
