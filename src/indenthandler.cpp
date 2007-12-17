5ff6bd983ba99ff24957f77f38c4ff598c3de64b
thomas_-_s <thomas_-_s@59b1889a-e5ac-428c-b0c7-476e01d41282>
2007-12-17 16:38:20 +0000

    [add] Feature Request ID 1852483 : If needed the shebang of the named indenter is tried to be read and the there named interpreter is used to call the indenter. http://universalindent.sf.net/issue/1852483
    git-svn-id: svn://svn.code.sf.net/p/universalindent/code/trunk@528 59b1889a-e5ac-428c-b0c7-476e01d41282

--- src/indenthandler.cpp
+++ src/indenthandler.cpp
@@ -340,7 +340,7 @@ QString IndentHandler::callExecutableIndenter(QString sourceCode, QString inputF
                 break;
         }
         processReturnString += tr("<br><b>Callstring was:</b> ") + indenterCompleteCallString;
-        processReturnString += tr("<br><br><b>Indenter output was:</b><pre> ") + indentProcess.readAll() + "</pre></html></body>";
+        processReturnString += tr("<br><br><b>Indenter output was:</b><pre>") + indentProcess.readAll() + "</pre></html></body>";
         QApplication::restoreOverrideCursor();
         errorMessageDialog->showMessage(tr("Error calling Indenter"), processReturnString);
     }
@@ -1011,14 +1011,56 @@ bool IndentHandler::createIndenterCallString() {
 
     foreach ( QString suffix, QStringList() << "" << ".exe" << ".bat" << ".com" << ".sh" ) {
         indenterExecutableSuffix = suffix;
-        indenterExecutableCallString = "\"" + QFileInfo(dataDirctoryStr).absoluteFilePath() + "/" + indenterFileName;
-        indenterExecutableCallString += suffix + "\"";
-        indentProcess.start( indenterExecutableCallString + " " + indenterShowHelpParameter );
-        if ( indentProcess.waitForFinished(2000) ) {
-            return true;
-        }
-        else if ( indentProcess.error() == QProcess::Timedout ) {
-            return true;
+        indenterExecutableCallString = QFileInfo(dataDirctoryStr).absoluteFilePath() + "/" + indenterFileName;
+        indenterExecutableCallString += suffix;
+
+        // Only try to call the indenter, if the file exists.
+        if ( QFile::exists(indenterExecutableCallString) ) {
+            indentProcess.start( "\"" + indenterExecutableCallString +  + "\" " + indenterShowHelpParameter );
+            if ( indentProcess.waitForFinished(2000) ) {
+                indenterExecutableCallString = "\"" + indenterExecutableCallString + "\"";
+                return true;
+            }
+            else if ( indentProcess.error() == QProcess::Timedout ) {
+                indenterExecutableCallString = "\"" + indenterExecutableCallString + "\"";
+                return true;
+            }
+            // If the file could not be executed, try to find a shebang at its start.
+            else {
+                // Try to open the file.
+                QFile indenterExecutable( indenterExecutableCallString );
+                if ( indenterExecutable.open(QFile::ReadOnly) ) {
+                    // Read the first line of the file.
+                    QTextStream indenterExecutableContent(&indenterExecutable);
+                    QString firstLineOfIndenterExe = indenterExecutableContent.readLine(75);
+                    indenterExecutable.close();
+
+                    // If the initial shebang is found, read the named intepreter. e.g. perl
+                    if ( firstLineOfIndenterExe.startsWith("#!") ) {
+                        // Get the rightmost word. by splitting the string into only full words.
+                        QString interpreterName = firstLineOfIndenterExe.split( QRegExp("\\W+") ).last();
+
+                        // Try to call the interpreter, if it exists.
+                        if ( !interpreterName.isEmpty() ) {
+                            indentProcess.start( interpreterName + " -h");
+                            if ( indentProcess.waitForFinished(2000) ) {
+                                indenterExecutableCallString = interpreterName + " \"" + indenterExecutableCallString + "\"";
+                                return true;
+                            }
+                            else if ( indentProcess.error() == QProcess::Timedout ) {
+                                indenterExecutableCallString = interpreterName + " \"" + indenterExecutableCallString + "\"";
+                                return true;
+                            }
+                            // now we know an interpreter is needed but it could not be called, so inform the user.
+                            else {
+                                errorMessageDialog->showMessage( tr("Interpreter needed"), 
+                                    tr("To use the selected indenter the program \"%1\" needs to be available in the global environment. You should add an entry to your path settings.").arg(interpreterName) );
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 
