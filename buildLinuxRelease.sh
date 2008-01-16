c10c85480f6d184770909d6dcc4626e70e5b47a0
thomas_-_s <thomas_-_s@59b1889a-e5ac-428c-b0c7-476e01d41282>
2008-01-16 12:28:57 +0000

    Adapted linux build shell script for new version.
    git-svn-id: svn://svn.code.sf.net/p/universalindent/code/trunk@599 59b1889a-e5ac-428c-b0c7-476e01d41282

--- buildLinuxRelease.sh
+++ buildLinuxRelease.sh
@@ -5,77 +5,88 @@ QTDIR=/usr/local/Trolltech/Qt-4.2.1
 export QTDIR
 PATH=$QTDIR/bin:$PATH
 export PATH
+
 echo "calling rm -r release/debug"
 echo "---------------------------"
 rm -r release
 rm -r debug
+
 echo "calling svn update"
 echo "------------------"
 #svn update
+
 echo "calling qmake"
 echo "-------------"
 qmake
+
 echo calling lrelease release
 echo ------------------------
 lrelease ./translations/universalindent_de.ts -qm ./translations/universalindent_de.qm
 lrelease ./translations/universalindent_zh_TW.ts -qm ./translations/universalindent_zh_TW.qm
 lrelease ./translations/universalindent_ja_JP.ts -qm ./translations/universalindent_ja_JP.qm
+
 echo calling make release
 echo --------------------
 make release
-echo calling rm -r UniversalIndentGUI_linux32
+
+echo calling rm -r universalindentgui_linux32
 echo ----------------------------------------
-rm -r UniversalIndentGUI_linux32
-echo calling mkdir UniversalIndentGUI_linux32
+rm -r universalindentgui_linux32
+
+echo calling mkdir universalindentgui_linux32
 echo ----------------------------------------
-mkdir UniversalIndentGUI_linux32
-mkdir UniversalIndentGUI_linux32/indenters
-mkdir UniversalIndentGUI_linux32/doc
-mkdir UniversalIndentGUI_linux32/translations
-echo copying files to UniversalIndentGUI_linux32
+mkdir universalindentgui_linux32
+mkdir universalindentgui_linux32/config
+mkdir universalindentgui_linux32/indenters
+mkdir universalindentgui_linux32/doc
+mkdir universalindentgui_linux32/translations
+
+echo copying files to universalindentgui_linux32
 echo ----------------------------------------
-cp ./CHANGELOG.txt ./UniversalIndentGUI_linux32/
-cp ./LICENSE.GPL ./UniversalIndentGUI_linux32/
-cp ./README.txt ./UniversalIndentGUI_linux32/
-cp ./INSTALL.txt ./UniversalIndentGUI_linux32/
-#cp ./indenters/.astylerc ./UniversalIndentGUI_linux32/indenters/
-cp ./indenters/astyle ./UniversalIndentGUI_linux32/indenters/
-cp ./indenters/astyle.html ./UniversalIndentGUI_linux32/indenters/
-#cp ./indenters/bcpp.cfg ./UniversalIndentGUI_linux32/indenters/
-cp ./indenters/bcpp ./UniversalIndentGUI_linux32/indenters/
-cp ./indenters/bcpp.txt ./UniversalIndentGUI_linux32/indenters/
-cp ./indenters/csstidy ./UniversalIndentGUI_linux32/indenters/
-#cp ./indenters/gc.cfg ./UniversalIndentGUI_linux32/indenters/
-cp ./indenters/gc.exe ./UniversalIndentGUI_linux32/indenters/
-cp ./indenters/gc.txt ./UniversalIndentGUI_linux32/indenters/
-cp ./indenters/tidy ./UniversalIndentGUI_linux32/indenters/
-#cp ./indenters/.indent.pro ./UniversalIndentGUI_linux32/indenters/
-cp ./indenters/indent ./UniversalIndentGUI_linux32/indenters/
-cp ./indenters/indent.html ./UniversalIndentGUI_linux32/indenters/
-#cp ./indenters/uncrustify.cfg ./UniversalIndentGUI_linux32/indenters/
-cp ./indenters/uncrustify ./UniversalIndentGUI_linux32/indenters/
-cp ./indenters/uncrustify.txt ./UniversalIndentGUI_linux32/indenters/
-cp ./indenters/uigui_astyle.ini ./UniversalIndentGUI_linux32/indenters/
-cp ./indenters/uigui_bcpp.ini ./UniversalIndentGUI_linux32/indenters/
-cp ./indenters/uigui_csstidy.ini ./UniversalIndentGUI_linux32/indenters/
-cp ./indenters/uigui_greatcode.ini ./UniversalIndentGUI_linux32/indenters/
-cp ./indenters/uigui_phpCB.ini ./UniversalIndentGUI_linux32/indenters/
-cp ./indenters/uigui_gnuindent.ini ./UniversalIndentGUI_linux32/indenters/
-cp ./indenters/uigui_uncrustify.ini ./UniversalIndentGUI_linux32/indenters/
-cp ./indenters/uigui_tidy.ini ./UniversalIndentGUI_linux32/indenters/
-cp ./indenters/highlighter.ini ./UniversalIndentGUI_linux32/indenters/
-cp ./indenters/example.cpp ./UniversalIndentGUI_linux32/indenters/
+cp ./CHANGELOG.txt ./universalindentgui_linux32/
+cp ./LICENSE.GPL ./universalindentgui_linux32/
+cp ./README.txt ./universalindentgui_linux32/
+cp ./INSTALL.txt ./universalindentgui_linux32/
+
+#cp ./indenters/.astylerc ./universalindentgui_linux32/indenters/
+cp ./indenters/astyle ./universalindentgui_linux32/indenters/
+cp ./indenters/astyle.html ./universalindentgui_linux32/indenters/
+#cp ./indenters/bcpp.cfg ./universalindentgui_linux32/indenters/
+cp ./indenters/bcpp ./universalindentgui_linux32/indenters/
+cp ./indenters/bcpp.txt ./universalindentgui_linux32/indenters/
+cp ./indenters/csstidy ./universalindentgui_linux32/indenters/
+#cp ./indenters/gc.cfg ./universalindentgui_linux32/indenters/
+cp ./indenters/gc.exe ./universalindentgui_linux32/indenters/
+cp ./indenters/gc.txt ./universalindentgui_linux32/indenters/
+#cp ./indenters/.indent.pro ./universalindentgui_linux32/indenters/
+cp ./indenters/indent ./universalindentgui_linux32/indenters/
+cp ./indenters/indent.html ./universalindentgui_linux32/indenters/
+cp ./indenters/JsDecoder.js ./universalindentgui_linux32/indenters/
+cp ./indenters/perltidy ./universalindentgui_linux32/indenters/
+cp ./indenters/PerlTidyLib.pm ./universalindentgui_linux32/indenters/
+cp ./indenters/shellindent.awk ./universalindentgui_linux32/indenters/
+cp ./indenters/tidy ./universalindentgui_linux32/indenters/
+#cp ./indenters/uncrustify.cfg ./universalindentgui_linux32/indenters/
+cp ./indenters/uncrustify ./universalindentgui_linux32/indenters/
+cp ./indenters/uncrustify.txt ./universalindentgui_linux32/indenters/
+
+cp ./indenters/uigui_*.ini ./universalindentgui_linux32/indenters/
+
+cp ./indenters/example.* ./universalindentgui_linux32/indenters/
+
+cp ./config/UiGuiSyntaxHighlightConfig.ini ./universalindentgui_linux32/config/
 
 # Copy the translation files.
 cp $QTDIR/translations/qt_de.qm ./translations/
 cp $QTDIR/translations/qt_ja_jp.qm ./translations/qt_ja_JP.qm
-cp ./translations/*.qm ./UniversalIndentGUI_linux32/translations/
+cp ./translations/*.qm ./universalindentgui_linux32/translations/
 
-cp ./doc/iniFileFormat.html ./UniversalIndentGUI_linux32/doc/
-cp ./release/UniversalIndentGUI ./UniversalIndentGUI_linux32/
-cd UniversalIndentGUI_linux32
-echo compressing UniversalIndentGUI_linux32
-echo ----------------------------------------
-tar czf UniversalIndentGUI_0.6.1_Beta_linux32.tgz *
-cd ..
+cp ./doc/iniFileFormat.html ./universalindentgui_linux32/doc/
+cp ./release/universalindentgui ./universalindentgui_linux32/
+
+#cd universalindentgui_linux32
+echo compressing universalindentgui_linux32
+echo --------------------------------------
+tar czf universalindentgui_0.8.0_beta_linux32.tgz universalindentgui_linux32
+#cd ..
 read -p "press any key to continue"
