dacf9a1df0f221c47bd2ae5ec4704493116ac4a0
thomas_-_s <thomas_-_s@59b1889a-e5ac-428c-b0c7-476e01d41282>
2008-09-25 16:55:07 +0000

    Since this class shall be completely static, the default constructor needed to be made private.
    git-svn-id: svn://svn.code.sf.net/p/universalindent/code/trunk@777 59b1889a-e5ac-428c-b0c7-476e01d41282

--- src/SettingsPaths.h
+++ src/SettingsPaths.h
@@ -37,6 +37,8 @@ public:
     static bool getPortableMode();
 
 private:
+    SettingsPaths();
+    
     static bool alreadyInitialized;
     static QString applicationBinaryPath;
     static QString settingsPath;
