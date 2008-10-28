63476e58a4b41625ad602efe4e273f64a0e8eb4e
thomas_-_s <thomas_-_s@59b1889a-e5ac-428c-b0c7-476e01d41282>
2008-10-28 18:18:51 +0000

    Corrected the shell script example. Seemed to have a line ending problem.
    git-svn-id: svn://svn.code.sf.net/p/universalindent/code/trunk@876 59b1889a-e5ac-428c-b0c7-476e01d41282

--- indenters/example.sh
+++ indenters/example.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
+string="Hallo Welt"
 # if else test
 if [ -n "$string" ]; then
 echo "The string is \"$string\"!"
@@ -8,11 +9,13 @@ echo "The string is empty!"
 fi
 
 # for test
+array="Text1 Text2 Text3 Text4"
 for i in $array
 do
 echo "The string \"$i\" is in the array!"
 done
 
+count=0
 while [ $count -le 10 ]
 do
 echo "We've counted up to $count."
@@ -21,3 +24,4 @@ done
 
 echo "Passed everything!"
 #read -p "press any key to continue"
+
