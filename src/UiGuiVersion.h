1a21ec6f72213b0fd790e9200fae7fbc56cd14d1
thomas_-_s <thomas_-_s@59b1889a-e5ac-428c-b0c7-476e01d41282>
2009-02-12 21:12:54 +0000

    Dummy commit for version update no 2. Did it wrong. Too late to do such things
    git-svn-id: svn://svn.code.sf.net/p/universalindent/code/trunk@929 59b1889a-e5ac-428c-b0c7-476e01d41282

--- src/UiGuiVersion.h
+++ src/UiGuiVersion.h
@@ -16,7 +16,7 @@
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
-
+//
 #ifndef UIGUIVERSION_H
 #define UIGUIVERSION_H
 
