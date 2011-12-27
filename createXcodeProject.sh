eb1197b72071921cf80329998a29eafa119af0b9
thomas_-_s <thomas_-_s@59b1889a-e5ac-428c-b0c7-476e01d41282>
2011-12-27 15:39:08 +0000

    createXcodeProject: Added some deploy notes.
    git-svn-id: svn://svn.code.sf.net/p/universalindent/code/trunk@1058 59b1889a-e5ac-428c-b0c7-476e01d41282

--- createXcodeProject.sh
+++ createXcodeProject.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
 qmake -spec macx-xcode UniversalIndentGUI.pro
+
+# Some notes on how to deploy on Mac using MacPorts Qt installation
+# macdeployqt ./release/UniversalIndentGUI.app
+# cp -rf /opt/local/lib/Resources/qt_menu.nib ./release/UniversalIndentGUI.app/Contents/Resources/
