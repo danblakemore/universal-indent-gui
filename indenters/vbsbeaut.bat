f689003f29f64efd95d6f49e58d5ad337e7c9003
thomas_-_s <thomas_-_s@59b1889a-e5ac-428c-b0c7-476e01d41282>
2011-12-28 22:53:35 +0000

    Added missing quotes and backslash to the VBS Beautifier helper batch file.
    git-svn-id: svn://svn.code.sf.net/p/universalindent/code/trunk@1059 59b1889a-e5ac-428c-b0c7-476e01d41282

--- indenters/vbsbeaut.bat
+++ indenters/vbsbeaut.bat
@@ -1,7 +1,8 @@
 @echo off
 set callpath=%0
+::echo %0
 set exeBasePath=%callpath:~0,-13%
 ::echo %exeBasePath%
-copy %exeBasePath%vbsbeaut_keywords.txt" .\keywords.txt > nul
-copy %exeBasePath%vbsbeaut_keywords_indent.txt" .\keywords_indent.txt > nul
-%exeBasePath%vbsbeaut.exe" %1 %2 %3 %4 %5 %6 %7
+copy "%exeBasePath%\vbsbeaut_keywords.txt" .\keywords.txt > nul
+copy "%exeBasePath%\vbsbeaut_keywords_indent.txt" .\keywords_indent.txt > nul
+"%exeBasePath%\vbsbeaut.exe" %1 %2 %3 %4 %5 %6 %7
