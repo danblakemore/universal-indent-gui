f014661ec43c04dadeb06a59c74f5505d26cec29
thomas_-_s <thomas_-_s@59b1889a-e5ac-428c-b0c7-476e01d41282>
2012-01-01 15:30:46 +0000

    Updated the version and release date.
    git-svn-id: svn://svn.code.sf.net/p/universalindent/code/trunk@1070 59b1889a-e5ac-428c-b0c7-476e01d41282

--- src/UiGuiVersion.h
+++ src/UiGuiVersion.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2006-2011 by Thomas Schweitzer                          *
+ *   Copyright (C) 2006-2012 by Thomas Schweitzer                          *
  *   thomas-schweitzer(at)arcor.de                                         *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -23,10 +23,10 @@
 class QString;
 
 // Define the version number here. Update this as the last file before a release.
-#define PROGRAM_VERSION 1.1.0
-#define PROGRAM_VERSION_STRING "1.1.0"
-#define RESOURCE_VERSION 1,1,0,0
-#define RESOURCE_VERSION_STRING "1,1,0,0\0"
+#define PROGRAM_VERSION 1.2.0
+#define PROGRAM_VERSION_STRING "1.2.0"
+#define RESOURCE_VERSION 1,2,0,0
+#define RESOURCE_VERSION_STRING "1,2,0,0\0"
 #define PROGRAM_REVISION "$Revision$"
 
 namespace UiGuiVersion {
